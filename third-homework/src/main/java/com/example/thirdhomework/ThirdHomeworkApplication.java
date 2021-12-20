5e0792b96f463d04dd6d0321ecdd56accea9ff2c
sbodur <sbodur@mail.com>
2021-12-20 14:07:45 +0300

    pom xml was created with suitable dependencies

---
+++ third-homework/src/main/java/com/example/thirdhomework/ThirdHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdHomeworkApplication.class, args);
+    }
+
+}
---
+++ third-homework/src/test/java/com/example/thirdhomework/ThirdHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thirdhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
